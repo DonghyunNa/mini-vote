c6ab6aafedaf21e14477256b461e551e71aecbe6
DonghyunNa <Donghyunna@quescompany.com>
2022-11-23 14:24:12 +0900

    Fix favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -19,6 +19,10 @@ export default class RootDocument extends Document {
             crossOrigin='anonymous'
             referrerPolicy='no-referrer'
           />
+          <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
+          <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
+          <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
+          <link rel='manifest' href='/site.webmanifest' />
         </Head>
         <Seo />
         <body>
